e18231eb691c0bca2144630f24c48695c983a0c0
Juho Härme <juho.harme@gmail.com>
2018-03-01 14:30:20 +0200

    Version with params

--- get_frequency_list.php
+++ get_frequency_list.php
@@ -12,10 +12,34 @@ require_once '../session_functions.php';
 require_once '../interface_functions.php';
 
 $dbname = "pest_inter";
+$codes = Array();
+foreach($_GET["codes"] as $code){
+    $codes[] = "'$code'";
+}
+$valuelist = implode(', ', $codes);
+$query = "SELECT startaddr, finaddr FROM librarysrc WHERE code IN ($valuelist)";
+$conn = open_connection($dbname);
+$result = pg_query($query)
+    or die(pg_last_error());
+$addresses = pg_fetch_all($result);
+
+
 $con_corpus = open_connection($dbname);
-$result = pg_query_params($con_corpus, "SELECT DISTINCT title, code FROM librarysrc WHERE lang = $1",Array($_GET["lang"]));
-$texts = pg_fetch_all($result);
-echo json_encode($texts);
+
+$stopwords = Array("and","this");
+$stopw_string = implode(", ", $stopwords);
+foreach($addresses as $address){
+    $result = pg_query_params($con_corpus, "SELECT token FROM {$_GET["language_table"]} 
+                                WHERE funct = $1 AND id between $2 AND $3
+                                AND token not in ({$stopw_string})
+                                ",
+                               Array("word",$address["startaddr"],$address["finaddr"]));
+    var_dump($result);
+}
+
+pg_close($conn);
+#
+#echo json_encode($texts);
 
 
 ?>
