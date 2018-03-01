57bd6aa0b11d5ad1a672d004e180567edde6a8b9
Juho Härme <juho.harme@gmail.com>
2018-03-01 14:53:06 +0200

    First rough working version with freq lists

--- get_frequency_list.php
+++ get_frequency_list.php
@@ -19,27 +19,33 @@ foreach($_GET["codes"] as $code){
 $valuelist = implode(', ', $codes);
 $query = "SELECT startaddr, finaddr FROM librarysrc WHERE code IN ($valuelist)";
 $conn = open_connection($dbname);
-$result = pg_query($query)
-    or die(pg_last_error());
+$result = pg_query($query) or die(pg_last_error());
 $addresses = pg_fetch_all($result);
+pg_close($conn);
 
 
 $con_corpus = open_connection($dbname);
 
-$stopwords = Array("and","this");
-$stopw_string = implode(", ", $stopwords);
+#$words = Array();
+
+$pos_table = "pos_{$_GET["lang"]}";
+$lemma_table = "lemma_{$_GET["lang"]}";
+$all_ids = Array();
 foreach($addresses as $address){
-    $result = pg_query_params($con_corpus, "SELECT token FROM {$_GET["language_table"]} 
-                                WHERE funct = $1 AND id between $2 AND $3
-                                AND token not in ({$stopw_string})
-                                ",
-                               Array("word",$address["startaddr"],$address["finaddr"]));
-    var_dump($result);
+    $result = pg_query_params($con_corpus, "SELECT linktotext FROM $pos_table WHERE pos = $1 AND id between $2 AND $3",
+                               Array("NN",$address["startaddr"],$address["finaddr"]));
+    $all_ids = array_merge($all_ids, pg_fetch_all_columns($result));
 }
 
+
+$conn = open_connection($dbname);
+$idstring = implode(', ', $all_ids);
+$query = "SELECT lemma, count(*) FROM $lemma_table WHERE linktotext IN ($idstring) Group By lemma ORDER BY count DESC";
+$result = pg_query($query) or die(pg_last_error());
+$freqs = pg_fetch_all($result);
 pg_close($conn);
-#
-#echo json_encode($texts);
+
+echo json_encode($freqs);
 
 
 ?>
