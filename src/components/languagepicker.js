048d43d8e332ad171133d56c851a13f41e6a4f42
Juho Härme <juho.harme@gmail.com>
2019-01-08 20:53:17 +0200

    Funcitionalized som components

---
+++ src/components/languagepicker.js
@@ -0,0 +1,13 @@
+import React from  'react'
+import {pickLanguage} from '../redux/actions.js'
+
+export default ( {corpus, dispatch}) => (
+
+        <select onChange={ (ev) => dispatch(pickLanguage(ev.target.value))}>
+                { 
+                    corpus.available_languages.map((lang, id) => <option key={id}>{lang}</option>)
+                }
+        </select>
+)
+
+
